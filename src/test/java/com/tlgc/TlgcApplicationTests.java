366be79b140fbce5402069ebf38b7d6f6f06d5d1
TONY <57581600@qq.com>
2017-11-17 23:17:08 +0800

    Initial commit

---
+++ src/test/java/com/tlgc/TlgcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tlgc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TlgcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
