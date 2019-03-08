554668983a2d7d37ac0e6a03bda1aa23170a9797
Tony Wu <57581600@qq.com>
2019-03-08 18:23:07 +0800

    replace 市

--- src/main/java/com/tlgc/mapper/GymMapper.java
+++ src/main/java/com/tlgc/mapper/GymMapper.java
@@ -24,9 +24,9 @@ public interface GymMapper {
     @Select("select prov,city,Id,name,phone,email,YYEmail,coordinate,tip,addr,dtPreSale,dtOpen,server,appId,appKey,isnull(nullif(app_signature,''),'小小运动馆')app_signature from TLG_Gym where id=#{id}")
     public Gym findGym(@Param("id") String id);
 
-    @Insert("insert into TLG_City(ProvinceId,CH_Name,EN_Name,Sort,Status)select id,#{city},[dbo].[fn_GetPinyin](replace(#{city},'市','')),0,2 from TLG_Province p join (select 1 x)x on p.CH_Name like '${prov}%' left join TLG_City c on c.ProvinceId=p.id and charindex(c.CH_Name,#{city})>0 where c.M_Id is null;" +
+    @Insert("insert into TLG_City(ProvinceId,CH_Name,EN_Name,Sort,Status)select id,replace(#{city},'市',''),[dbo].[fn_GetPinyin](replace(#{city},'市','')),0,2 from TLG_Province p join (select 1 x)x on p.CH_Name like '${prov}%' left join TLG_City c on c.ProvinceId=p.id and charindex(c.CH_Name,#{city})>0 where c.M_Id is null;" +
             "delete from tlg_gym where Id=#{Id};insert into tlg_gym(Id,CH_Name,prov,city,cityId,phone,fax,email,YYEmail,coordinate,tip,addr,dtPreSale,dtOpen,updateTime,status,server,appId,appKey,app_signature,isPreparing)" +
-            "select #{Id},#{CH_Name},'${prov}',#{city},M_Id,#{phone},#{fax},#{email},#{YYEmail},#{coordinate},#{tip},#{addr},#{dtPreSale},#{dtOpen},getdate(),#{status},#{server},#{appId},#{appKey},#{app_signature},#{isPreparing} from TLG_City where charindex(CH_Name,#{city})>0")
+            "select #{Id},#{CH_Name},#{prov},#{city},M_Id,#{phone},#{fax},#{email},#{YYEmail},#{coordinate},#{tip},#{addr},#{dtPreSale},#{dtOpen},getdate(),#{status},#{server},#{appId},#{appKey},#{app_signature},#{isPreparing} from TLG_City where charindex(CH_Name,#{city})>0")
     public Integer saveGym(Gym gym);
 
 }
