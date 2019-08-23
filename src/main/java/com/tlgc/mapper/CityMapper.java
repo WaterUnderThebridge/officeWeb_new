f4ddfd55a3230ebec2220c5d753ef5107245045c
Tony Wu <57581600@qq.com>
2019-08-23 20:31:36 +0800

    city -1

--- src/main/java/com/tlgc/mapper/CityMapper.java
+++ src/main/java/com/tlgc/mapper/CityMapper.java
@@ -11,8 +11,9 @@ import java.util.List;
  * Created by TONY on 2017/11/19.
  */
 public interface CityMapper {
-    @Select("select top 1 M_Id Id,CH_Name from TLG_City where charindex(CH_Name,#{CH_Name})>0")
+    @Select("select top 1 M_Id Id,CH_Name from TLG_City where status=2 and charindex(CH_Name,#{CH_Name})>0")
     public City getCity(@Param("CH_Name") String CH_Name);
-    @Select("select  M_Id Id,CH_Name from TLG_City where ProvinceId=#{ProvinceId}")
+    @Select("select ProvinceId,M_Id Id,CH_Name from TLG_City where status=2 and ProvinceId=#{ProvinceId} " +
+            "union all select ProvinceId,M_Id Id,CH_Name from TLG_City where status=2 and -1=#{ProvinceId} order by ProvinceId")
     public List<City> getAllByProvinceId(@Param("ProvinceId") Integer ProvinceId);
 }
