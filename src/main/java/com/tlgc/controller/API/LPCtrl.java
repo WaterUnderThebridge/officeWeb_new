6dc6459b482e0613716d6c92cc39d096ce26837a
TonyWu <wu.yong@thelittlegym.com.cn>
2020-02-07 23:04:09 +0800

    sync lp account

--- src/main/java/com/tlgc/controller/API/LPCtrl.java
+++ src/main/java/com/tlgc/controller/API/LPCtrl.java
@@ -1,12 +1,18 @@
 package com.tlgc.controller.API;
 
+import com.fasterxml.jackson.annotation.JsonView;
 import com.tlgc.entity.EmployeeNonOMSGym;
+import com.tlgc.entity.Result;
 import com.tlgc.mapper.lessonplan.AccontMapper;
+import com.tlgc.utils.ResultUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by Tony on 2020/2/7.
@@ -19,9 +25,30 @@ public class LPCtrl {
     AccontMapper accontMapper;
 
 
+    @GetMapping("/getAccounts")
+    @JsonView(EmployeeNonOMSGym.AccountDetail.class)
+    public List<EmployeeNonOMSGym> accounts() {
+        List<EmployeeNonOMSGym> employeeNonOMSGyms = accontMapper.getAccounts();
+        return employeeNonOMSGyms;
+    }
     @GetMapping("/getAccount")
+    @JsonView(EmployeeNonOMSGym.AccountDetail.class)
     public EmployeeNonOMSGym account() {
-        EmployeeNonOMSGym employeeNonOMSGym = accontMapper.getAccount("500005");
-        return employeeNonOMSGym;
+        EmployeeNonOMSGym employeeNonOMSGyms = accontMapper.getAccount("500005");
+        return employeeNonOMSGyms;
+    }
+
+    @RequestMapping(value = "/saveAccount")
+    public Result saveAccount(@RequestParam(value = "callback", required = false) String callback,
+                              @RequestParam(value = "gymCode", required = false) String gymCode,
+                              @RequestParam(value = "gymName", required = false) String gymName,
+                              @RequestParam(value = "city", required = false) String city,
+                              @RequestParam(value = "isActive", required = false) Byte isActive)
+    {
+        Integer res = accontMapper.saveAccount(gymName,gymCode,city,isActive);
+        if(res>0) {
+            return ResultUtil.success();
+        }
+        return ResultUtil.error();
     }
 }
--- src/main/java/com/tlgc/mapper/lessonplan/AccontMapper.java
+++ src/main/java/com/tlgc/mapper/lessonplan/AccontMapper.java
@@ -3,9 +3,18 @@ package com.tlgc.mapper.lessonplan;
 import com.tlgc.entity.EmployeeNonOMSGym;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.HashMap;
+import java.util.List;
+
 /**
  * Created by Tony on 2020/2/7.
  */
-public interface   AccontMapper {
+public interface  AccontMapper {
     EmployeeNonOMSGym getAccount(@Param("gymCode") String gymCode);
+
+    List<EmployeeNonOMSGym> getAccounts();
+
+    Integer saveAccount(@Param("gymName") String gymName,@Param("gymCode") String gymCode,@Param("city") String city,@Param("isActive") Byte isActive);
+
+
 }
